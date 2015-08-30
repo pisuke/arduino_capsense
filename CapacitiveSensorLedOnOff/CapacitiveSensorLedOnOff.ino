2b62bd92ed8e147c31790cf1aca9eb47f183ad3f
Francesco Anselmo <francesco.anselmo@gmail.com>
2015-08-31 00:53:15 +0100

    Capacitive sensing sketch now working OK

--- CapacitiveSensorLedOnOff/CapacitiveSensorLedOnOff.ino
+++ CapacitiveSensorLedOnOff/CapacitiveSensorLedOnOff.ino
@@ -2,56 +2,112 @@
 
 
 /*
- * CapitiveSense Library Demo Sketch
- * Paul Badger 2008
+ * Capacitive sensor switch for London Design Festival 2015 - 41 A House for London
+ * Francesco Anselmo francesco.anselmo@arup.com
  * Uses a high value resistor e.g. 10M between send pin and receive pin
  * Resistor effects sensitivity, experiment with values, 50K - 50M. Larger resistor values yield larger sensor values.
  * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
  */
-int ledPin =  11;
-int state = LOW;
+
+int threshold = 60;
+
+int ledPin1 =  8;
+int state1 = LOW;
+int ledPin2 =  9;
+int state2 = LOW;
+int ledPin3 =  10;
+int state3 = LOW;
+int ledPin4 =  11;
+int state4 = LOW;
+int ledPin5 =  12;
+int state5 = LOW;
 int count = 1;
 
-CapacitiveSensor   cs_2_4 = CapacitiveSensor(2, 4);       // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
-//CapacitiveSensor   cs_2_6 = CapacitiveSensor(2, 6);       // 10M resistor between pins 4 & 6, pin 6 is sensor pin, add a wire and or foil
-//CapacitiveSensor   cs_2_8 = CapacitiveSensor(2, 8);       // 10M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foil
+CapacitiveSensor cs_2_3 = CapacitiveSensor(2, 3);  // 10M resistor between pins 3 & 2, pin 3 is sensor pin, add a wire and or foil if desired
+CapacitiveSensor cs_2_4 = CapacitiveSensor(2, 4);  // 10M resistor between pins 3 & 2, pin 4 is sensor pin, add a wire and or foil if desired
+CapacitiveSensor cs_2_5 = CapacitiveSensor(2, 5);  // 10M resistor between pins 3 & 2, pin 5 is sensor pin, add a wire and or foil if desired
+CapacitiveSensor cs_2_6 = CapacitiveSensor(2, 6);  // 10M resistor between pins 3 & 2, pin 6 is sensor pin, add a wire and or foil if desired
+CapacitiveSensor cs_2_7 = CapacitiveSensor(2, 7);  // 10M resistor between pins 3 & 2, pin 7 is sensor pin, add a wire and or foil if desired
+
 
 void setup()
 {
   //cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
+  pinMode(ledPin5, OUTPUT);
 }
 
 void loop()
 {
   long start = millis();
-  long total1 =  cs_2_4.capacitiveSensor(30);
-  //long total2 =  cs_2_6.capacitiveSensor(30);
-  //long total3 =  cs_2_8.capacitiveSensor(30);
-//sensorState = digitalRead(capacitiveSensor);
+  long total1 =  cs_2_3.capacitiveSensor(30);
+  long total2 =  cs_2_4.capacitiveSensor(30);
+  long total3 =  cs_2_5.capacitiveSensor(30);
+  long total4 =  cs_2_6.capacitiveSensor(30);
+  long total5 =  cs_2_7.capacitiveSensor(30);
 
   Serial.print(millis() - start);        // check on performance in milliseconds
   Serial.print("\t");                    // tab character for debug windown spacing
 
-  Serial.print(total1);                  // print sensor output 1
-  Serial.print("\t");
-  //Serial.print("total2");
-  //Serial.print(total2);                  // print sensor output 2
-  //Serial.println("\t");
-  //Serial.print("total3");
- // Serial.println(total3);                // print sensor output 3
-  if (total1 > 20 && state == LOW) {// || total2 > 99 || total3 > 99 && state == LOW) {//originally 200 for both low and high
-    state = HIGH;
+  printSerial(total1, state1, ledPin1);
+  printSerial(total2, state2, ledPin2);
+  printSerial(total3, state3, ledPin3);
+  printSerial(total4, state4, ledPin4);
+  printSerial(total5, state5, ledPin5);
 
-    digitalWrite (ledPin,  !digitalRead(ledPin));  //LED on
+  Serial.println("");
 
+  processCapSensor(total1, state1, ledPin1);
+  processCapSensor(total2, state2, ledPin2);
+  processCapSensor(total3, state3, ledPin3);
+  processCapSensor(total4, state4, ledPin4);
+  processCapSensor(total5, state5, ledPin5);
+
+  /*
+  if (total1 > threshold && state1 == LOW) {// || total2 > 99 || total3 > 99 && state == LOW) {//originally 200 for both low and high
+    state1 = HIGH;
+    digitalWrite (ledPin1, !digitalRead(ledPin1));  //LED on
+    //digitalWrite (ledPin1, state1);  
   }
   
- if (total1 < 20 && state == HIGH)   // only set the state low when the cap sense is really lower 
+ if (total1 < threshold && state1 == HIGH)   // only set the state low when the cap sense is really lower 
   {
-    state = LOW;
+    state1 = LOW;
+    //digitalWrite (ledPin1, !digitalRead(ledPin1));  //LED off
+    //digitalWrite (ledPin1, state1);  
   } 
+  */
  
   delay(100);                             // arbitrary delay to limit data to serial port
 }
+
+void printSerial(int total, int state, int ledPin) {
+  Serial.print(total);                  // print sensor output 1
+  Serial.print("\t");
+  Serial.print(state);                  // print sensor output 1
+  Serial.print("\t");
+  Serial.print(digitalRead(ledPin));                  // print sensor output 1
+  Serial.print("\t");
+}
+
+void processCapSensor(int total, int state, int ledPin) {
+  if (total > threshold && state == LOW) {
+    state = HIGH;
+    digitalWrite (ledPin, !digitalRead(ledPin));  
+    //digitalWrite (ledPin, state);  
+  }
+  
+ if (total < threshold && state == HIGH)   
+  {
+    state = LOW;
+    //digitalWrite (ledPin, !digitalRead(ledPin));  //LED off
+    //digitalWrite (ledPin, state);  
+  } 
+}
+
+
+
